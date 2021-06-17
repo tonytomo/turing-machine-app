69b2e8121cdf2326c251cc49affd4823f9018790
jambanhanyut <colonelmacmillan@gmail.com>
2021-06-17 21:45:00 +0700

    meringkas subtraction dan multiplication

--- js/ops/times.js
+++ js/ops/times.js
@@ -8,365 +8,39 @@ function timesMove() {
             crAct[i].className = crAct[i].className.replace(" active", "");
         }
 
-        // STATE 0
-        if (state == 0) {
-            // 0, 0 / R
-            if (opsLine[it].content == "0") {
-                // Activate block
-                opsTmLine.childNodes[it].className += " active";
-                opsTmLine.childNodes[it + 1].scrollIntoView(false);
+        done = 0;
 
-                // Ke kanan
-                it++;
-            }
-            // C, C / R
-            else if (opsLine[it].content == "C") {
-                // Activate block
-                opsTmLine.childNodes[it].className += " active";
-                opsTmLine.childNodes[it + 1].scrollIntoView(false);
-
-                // Change state
-                state = 1;
-
-                // Ke kanan
-                it++;
-            }
-        }
-
-        // STATE 1
-        else if (state == 1) {
-            // 0, 0 / R
-            if (opsLine[it].content == "0") {
-                // Activate block
-                opsTmLine.childNodes[it].className += " active";
-                opsTmLine.childNodes[it + 1].scrollIntoView(false);
-
-                // Ke kanan
-                it++;
-            }
-            // B, C / L
-            else if (opsLine[it].content == "B") {
-                // Add blank block
-                opsLine.push(new Blockops("B"));
-
-                // Change textContent
-                opsLine[it].changeTo("C");
-                opsTmLine.childNodes[it].textContent = "C";
-
-                // Activate block
-                opsTmLine.childNodes[it].className += " active";
-                opsTmLine.childNodes[it + 1].scrollIntoView(false);
-
-                // Change state
-                state = 2;
-
-                // Ke kiri
-                it--;
-            }
-        }
-
-        // STATE 2
-        else if (state == 2) {
-            // 0, 0 / L
-            if (opsLine[it].content == "0") {
-                // Activate block
-                opsTmLine.childNodes[it].className += " active";
-                opsTmLine.childNodes[it + 1].scrollIntoView(false);
-
-                // Ke kiri
-                it--;
-            }
-            // C, C / R
-            else if (opsLine[it].content == "C") {
-                // Activate block
-                opsTmLine.childNodes[it].className += " active";
-                opsTmLine.childNodes[it + 1].scrollIntoView(false);
-
-                // Change state
-                state = 3;
-
-                // Ke kanan
-                it++;
-            }
-        }
-
-        // STATE 3
-        else if (state == 3) {
-            // X, X / R
-            if (opsLine[it].content == "X") {
-                // Activate block
-                opsTmLine.childNodes[it].className += " active";
-                opsTmLine.childNodes[it - 1].scrollIntoView(false);
-
-                // Ke kanan
-                it++;
-            }
-            // 0, X / L
-            else if (opsLine[it].content == "0") {
-                // Change textContent
-                opsLine[it].changeTo("X");
-                opsTmLine.childNodes[it].textContent = "X";
-
-                // Activate block
-                opsTmLine.childNodes[it].className += " active";
-                opsTmLine.childNodes[it - 1].scrollIntoView(false);
-
-                // Change state
-                state = 4;
-
-                // Ke kiri
-                it--;
-            }
-            // C, B / R
-            else if (opsLine[it].content == "C") {
-                // Change textContent
-                opsLine[it].changeTo("B");
-                opsTmLine.childNodes[it].textContent = "B";
-
-                // Activate block
-                opsTmLine.childNodes[it].className += " active";
-                opsTmLine.childNodes[it - 1].scrollIntoView(false);
-
-                // Change state
-                state = 12;
-
-                // Ke kanan
-                it++;
-            }
-        }
-
-        // STATE 4
-        else if (state == 4) {
-            // X, X / L
-            if (opsLine[it].content == "X") {
-                // Activate block
-                opsTmLine.childNodes[it].className += " active";
-                opsTmLine.childNodes[it - 1].scrollIntoView(false);
-
-                // Ke kiri
-                it--;
-            }
-            // C, C / L
-            else if (opsLine[it].content == "C") {
-                // Activate block
-                opsTmLine.childNodes[it].className += " active";
-                opsTmLine.childNodes[it - 1].scrollIntoView(false);
-
-                // Change state
-                state = 5;
-
-                // Ke kiri
-                it--;
-            }
-        }
-
-        // STATE 5
-        else if (state == 5) {
-            // Y, Y / L
-            if (opsLine[it].content == "Y") {
-                // Activate block
-                opsTmLine.childNodes[it].className += " active";
-                opsTmLine.childNodes[it - 1].scrollIntoView(false);
-
-                // Ke kiri
-                it--;
-            }
-            // 0, Y / R
-            else if (opsLine[it].content == "0") {
-                // Change textContent
-                opsLine[it].changeTo("Y");
-                opsTmLine.childNodes[it].textContent = "Y";
-
-                // Activate block
-                opsTmLine.childNodes[it].className += " active";
-                opsTmLine.childNodes[it - 1].scrollIntoView(false);
-
-                // Change state
-                state = 6;
-
-                // Ke kanan
-                it++;
-            }
-            // B, B / R
-            else if (opsLine[it].content == "B") {
-                // Activate block
-                opsTmLine.childNodes[it].className += " active";
-                opsTmLine.childNodes[it - 1].scrollIntoView(false);
-
-                // Change state
-                state = 11;
-
-                // Ke kanan
-                it++;
-            }
-        }
-
-        // STATE 6
-        else if (state == 6) {
-            // Y, Y / R
-            if (opsLine[it].content == "Y") {
-                // Activate block
-                opsTmLine.childNodes[it].className += " active";
-                opsTmLine.childNodes[it + 1].scrollIntoView(false);
-
-                // Ke kanan
-                it++;
-            }
-            // C, C / R
-            else if (opsLine[it].content == "C") {
-                // Activate block
-                opsTmLine.childNodes[it].className += " active";
-                opsTmLine.childNodes[it + 1].scrollIntoView(false);
-
-                // Change state
-                state = 7;
-
-                // Ke kanan
-                it++;
-            }
-        }
-
-        // STATE 7
-        else if (state == 7) {
-            // 0, 0 / R || X, X / R
-            if (opsLine[it].content == "0" || opsLine[it].content == "X") {
-                // Activate block
-                opsTmLine.childNodes[it].className += " active";
-                opsTmLine.childNodes[it + 1].scrollIntoView(false);
-
-                // Ke kanan
-                it++;
-            }
-            // C, C / R
-            else if (opsLine[it].content == "C") {
-                // Activate block
-                opsTmLine.childNodes[it].className += " active";
-                opsTmLine.childNodes[it + 1].scrollIntoView(false);
-
-                // Change state
-                state = 8;
-
-                // Ke kanan
-                it++;
-            }
-        }
-
-        // STATE 8
-        else if (state == 8) {
-            // 0, 0 / R
-            if (opsLine[it].content == "0") {
-                // Activate block
-                opsTmLine.childNodes[it].className += " active";
-                opsTmLine.childNodes[it + 1].scrollIntoView(false);
-
-                // Ke kanan
-                it++;
-            }
-            // B, 0 / L
-            else if (opsLine[it].content == "B") {
-                // Add blank block
-                opsLine.push(new Blockops("B"));
-
-                // Change textContent
-                opsLine[it].changeTo("0");
-                opsTmLine.childNodes[it].textContent = "0";
-
-                // Result
-                opsTmLine.childNodes[it].className += " result";
-
-                // Activate block
-                opsTmLine.childNodes[it].className += " active";
-                opsTmLine.childNodes[it + 1].scrollIntoView(false);
-
-                // Change state
-                state = 9;
-
-                // Ke kiri
-                it--;
-            }
-        }
-
-        // STATE 9
-        else if (state == 9) {
-            // 0, 0 / L
-            if (opsLine[it].content == "0") {
-                // Activate block
-                opsTmLine.childNodes[it].className += " active";
-                opsTmLine.childNodes[it - 1].scrollIntoView(false);
-
-                // Ke kiri
-                it--;
-            }
-            // C, C / L
-            else if (opsLine[it].content == "C") {
-                // Activate block
-                opsTmLine.childNodes[it].className += " active";
-                opsTmLine.childNodes[it - 1].scrollIntoView(false);
-
-                // Change state
-                state = 10;
-
-                // Ke kiri
-                it--;
-            }
-        }
-
-        // STATE 10
-        else if (state == 10) {
-            // 0, 0 / L || X, X / L
-            if (opsLine[it].content == "0" || opsLine[it].content == "X") {
-                // Activate block
-                opsTmLine.childNodes[it].className += " active";
-                opsTmLine.childNodes[it - 1].scrollIntoView(false);
-
-                // Ke kiri
-                it--;
-            }
-            // C, C / L
-            else if (opsLine[it].content == "C") {
-                // Activate block
-                opsTmLine.childNodes[it].className += " active";
-                opsTmLine.childNodes[it - 1].scrollIntoView(false);
-
-                // Change state
-                state = 5;
-
-                // Ke kiri
-                it--;
-            }
-        }
-
-        // STATE 11
-        else if (state == 11) {
-            // Y, 0 / R
-            if (opsLine[it].content == "Y") {
-                // Change textContent
-                opsLine[it].changeTo("0");
-                opsTmLine.childNodes[it].textContent = "0";
-
-                // Activate block
-                opsTmLine.childNodes[it].className += " active";
-                opsTmLine.childNodes[it - 1].scrollIntoView(false);
-
-                // Ke kanan
-                it++;
-            }
-            // C, C / R
-            else if (opsLine[it].content == "C") {
-                // Activate block
-                opsTmLine.childNodes[it].className += " active";
-                opsTmLine.childNodes[it - 1].scrollIntoView(false);
-
-                // Change state
-                state = 3;
-
-                // Ke kanan
-                it++;
-            }
-        }
+        moving(0, 1, "C", "C", 1);
+        moving(0, 0, "0", "0", 1);
+        moving(1, 1, "0", "0", 1);
+        moving(1, 2, "B", "C", 0, 1);
+        moving(2, 2, "0", "0", 0);
+        moving(2, 3, "C", "C", 1);
+        moving(3, 3, "X", "X", 1);
+        moving(3, 4, "0", "X", 0);
+        moving(3, 12, "C", "B", 1);
+        moving(4, 4, "X", "X", 0);
+        moving(4, 5, "C", "C", 0);
+        moving(5, 5, "Y", "Y", 0);
+        moving(5, 6, "0", "Y", 1);
+        moving(5, 11, "B", "B", 1);
+        moving(6, 6, "Y", "Y", 1);
+        moving(6, 7, "C", "C", 1);
+        moving(7, 7, "0", "0", 1);
+        moving(7, 7, "X", "X", 1);
+        moving(7, 8, "C", "C", 1);
+        moving(8, 8, "0", "0", 1);
+        moving(8, 9, "B", "0", 0, 1);
+        moving(9, 9, "0", "0", 0);
+        moving(9, 10, "C", "C", 0);
+        moving(10, 10, "0", "0", 0);
+        moving(10, 10, "X", "X", 0);
+        moving(10, 5, "C", "C", 0);
+        moving(11, 11, "Y", "0", 1);
+        moving(11, 3, "C", "C", 1);
 
         // STATE 12 (FINAL STATE)
-        else if (state == 12) {
+        if (state == 12) {
             // Selesai
             stopOpsAuto();
             opsTmLine.childNodes[it + 1].scrollIntoView(false);
